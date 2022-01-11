066f301d43c9a9375889d93ecccca37b1d1aecc7
aibechelios <aibechelllo@gmail.com>
2022-01-11 21:34:19 +0600

    2nd project complete

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,17 @@
 /* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+
+var tagg = document.getElementsByClassName('collapsible__button')
+
+
+for (i = 0; i < tagg.length; i++) {
+    tagg[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if(content.style.display == "none"){
+            content.style.display = "block"
+        }else{
+            content.style.display = "none"
+        }
+
+    })
+}
